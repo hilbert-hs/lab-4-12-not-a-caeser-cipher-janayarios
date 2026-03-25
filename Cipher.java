190af230eed906f406ccca55917c7becc69695c7
janayarios <jesus_anaya57@icloud.com>
2026-03-25 16:04:12 +0000

    finished lab

--- Cipher.java
+++ Cipher.java
@@ -1,21 +1,39 @@
 class Cipher {
-  private String[][] leterBlock;
+  private String[][] letterBlock;
   private int numRows;
   private int numCols;
 
   // Constructor: parameters include number of rows, number of columns
 
   public Cipher(int r, int c) {
-
+    this.numRows = r;
+    this.numCols = c;
+    this.letterBlock = new String[r][c];
   }
+
 
   // method fillBlock(): places a String into letterBlock in row major order
   // @param str: the String to be processed
   // POSTCONDITION: if str.length() < numRows * numCols, "A" is placed in each unfilled cell
   // POSTCONDITION: if str.length() > numRows * numCols, trailing characters are ignored
 
-  private void fillBlock(String str) {
+  public void fillBlock(String str) {
+    int letterPosition = 0;
+    for (int i = 0; i < numRows; i++) {
+      
+      for (int j = 0; j < numCols; j++) {
+        if (letterPosition < str.length()) {
+          letterBlock[i][j] = str.substring(letterPosition, letterPosition + 1);
+          letterPosition++;
+        }
 
+        else {
+          letterBlock[i][j] = "A";
+        }
+
+      }
+
+    }
   }
 
   // method encryptBlock(): extracts encrypted message (cipher) from letterBlock in column major order
@@ -23,7 +41,20 @@ class Cipher {
   // @return encrypted String from letterBlock
 
   private String encryptBlock() {
-    return "";
+    String encryptedString = "";
+
+    for (int i = 0; i < numCols; i++) {
+
+      for (int j = 0; j < numRows; j++) {
+
+        encryptedString += letterBlock[j][i];
+
+      }
+
+    }
+
+    return encryptedString;
+
   }
 
   // method encryptMessage(): encrypts a message using methods fillBlock() and encryptBlock()
@@ -32,6 +63,8 @@ class Cipher {
   // if message is an empty String, returns an empty string
 
   public String encryptMessage(String message) {
-    return "";
+    fillBlock(message);
+    encryptBlock();
+    return encryptBlock();
   }
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ class main {
   public static void main(String[] args) {
 
   // write code here to test your Cipher class.
+  // Cipher test = new Cipher(2, 5);
+  // System.out.println(test.encryptMessage("Highschool"));
+
+
   // when you're finished, replace your test code with
   // the tests I provided
-
   }
 }
